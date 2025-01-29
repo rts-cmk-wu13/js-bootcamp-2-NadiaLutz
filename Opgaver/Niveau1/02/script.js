b969f71f7097e7d97a1e1dd64453eeef901ec757
Nadia Lutz <na.lutz@outlook.com>
2025-01-29 08:52:37 +0100

    made the functions to divide and made it print out in a <div>

---
+++ Opgaver/Niveau1/02/script.js
@@ -0,0 +1,12 @@
+
+function beregnForhold(x,y) {
+        let result = x / y;
+        return result
+}
+
+
+beregnForhold(4,3);
+
+let outerResult = beregnForhold(4,3)
+
+document.querySelector('.result').innerHTML = outerResult;
